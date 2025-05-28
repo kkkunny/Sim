9fdd8150a70ad590024a156a2a0c195823bea3b5
kkkunny <1401289859@qq.com>
2025-05-28 22:51:20 +0800

    feat: 更新llvm版本为20

--- compiler/config/llvm_config_windows.go
+++ compiler/config/llvm_config_windows.go
@@ -6,5 +6,5 @@ package config
 
 // #cgo CFLAGS: -IC:/msys64/clang64/include  -D_FILE_OFFSET_BITS=64 -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
 // #cgo CXXFLAGS: -IC:/msys64/clang64/include -std=c++17 -stdlib=libc++  -fno-exceptions -D_FILE_OFFSET_BITS=64 -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
-// #cgo LDFLAGS: -LC:/msys64/clang64/lib -lLLVM-19
+// #cgo LDFLAGS: -LC:/msys64/clang64/lib -lLLVM-20
 import "C"
