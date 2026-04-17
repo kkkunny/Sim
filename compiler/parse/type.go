8a0634f7628638afcf9530f1e9d4e54e444ec8c1
kkkunny <1401289859@qq.com>
2026-04-17 23:57:12 +0800

    feat: 新增函数类型

--- compiler/parse/type.go
+++ compiler/parse/type.go
@@ -1,11 +1,35 @@
 package parse
 
 import (
+	"github.com/kkkunny/stl/container/optional"
+
 	"github.com/kkkunny/Sim/compiler/ast"
 	"github.com/kkkunny/Sim/compiler/token"
 )
 
 func (p *Parser) parseType() ast.Type {
-	name := p.expect(token.KindEnum.Ident)
-	return &ast.IdentType{Name: name}
+	switch p.nextToken.Kind {
+	case token.KindEnum.Lpa:
+		begin := p.expect(token.KindEnum.Lpa).Position
+		p.skip(token.KindEnum.Br)
+		var params []ast.Type
+		for p.nextToken.Kind != token.KindEnum.Rpa {
+			params = append(params, p.parseType())
+			if !p.ifSkip(token.KindEnum.Comma) {
+				break
+			}
+			p.skip(token.KindEnum.Br)
+		}
+		p.expect(token.KindEnum.Rpa)
+
+		var returnType optional.Optional[ast.Type]
+		if p.ifSkip(token.KindEnum.Arrow) {
+			returnType = optional.Some(p.parseType())
+		}
+
+		return &ast.FuncType{BeginPosition: begin, Params: params, ReturnType: returnType, EndPosition: p.curToken.Position}
+	default:
+		name := p.expect(token.KindEnum.Ident)
+		return &ast.IdentType{Name: name}
+	}
 }
